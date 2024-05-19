26919993dd4eb29eac6cb7fad1cb0eb79cedeb4d
Younggyo Lee <52687834+younggyolee@users.noreply.github.com>
2024-05-19 17:28:42 +0900

    [wk4] mult + fill

--- projects/4/fill/Fill.asm
+++ projects/4/fill/Fill.asm
@@ -8,4 +8,62 @@
 // i.e. writes "black" in every pixel. When no key is pressed, 
 // the screen should be cleared.
 
-//// Replace this comment with your code.
+(LOOP)
+  // declare variable i
+  @i
+  M=0
+  // checks keyboard input
+  // if any, paint black
+  // else, paint white
+  @KBD
+  D=M
+  @BLACK
+  D;JGT
+  @WHITE
+  D;JEQ
+
+  (BLACK)
+    // break if i == 8192
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // draw into a cell
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=-1
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    @BLACK
+    0;JMP
+
+  (WHITE)
+    // break if i == 8192
+    @i
+    D=M
+    @8192
+    D=D-A
+    @LOOP
+    D;JEQ
+
+    // draw into a cell
+    @i
+    D=M
+    @SCREEN
+    A=A+D
+    M=0
+
+    // i = i + 1
+    @i
+    M=M+1
+
+    @WHITE
+    0;JMP
