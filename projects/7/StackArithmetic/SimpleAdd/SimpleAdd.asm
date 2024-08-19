de1cd03efaa748fc195a295a5f7c242d8777e032
Younggyo Lee <52687834+younggyolee@users.noreply.github.com>
2024-08-19 22:25:05 +0900

    [wk7] SimpleAdd.asm

---
+++ projects/7/StackArithmetic/SimpleAdd/SimpleAdd.asm
@@ -0,0 +1,33 @@
+@256
+D=A
+@SP
+M=D
+@7
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@8
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+@SP
+M=M-1
+@SP
+A=M
+M=D
+@SP
+M=M-1
+@SP
+A=M
+M=D+M
+@SP
+M=M+1
+(END)
+  @END
+  0;JMP
