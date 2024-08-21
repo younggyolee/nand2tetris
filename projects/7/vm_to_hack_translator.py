79e759fd595611cf86c6e3b2e7d75ff447100768
Younggyo Lee <52687834+younggyolee@users.noreply.github.com>
2024-08-21 23:08:45 +0900

    [wk7] vm to hack translator - stacktest done

--- projects/7/vm_to_hack_translator.py
+++ projects/7/vm_to_hack_translator.py
@@ -62,7 +62,7 @@ class Parser:
 
     def advance(self):
         self.current_line_number += 1
-    
+
     def current_line_type(self):
         l = self.current_line()
         if l.startswith('push'):
@@ -105,22 +105,110 @@ class Parser:
             # TODO
             pass
 
-    def handle_arithmetic(self, line, hack_writer: HackWriter):
-        if line == 'add':
+    def assign_0_to_sp_mem(self, hack_writer: HackWriter):
+        hack_writer.add_line('@SP')
+        hack_writer.add_line('A=M')
+        hack_writer.add_line('M=0')
+
+    def assign_minus_one_to_sp_mem(self, hack_writer: HackWriter):
+        hack_writer.add_line('@SP')
+        hack_writer.add_line('A=M')
+        hack_writer.add_line('M=-1')
+
+    # def assign_val_to_sp_mem(self, val, hack_writer: HackWriter):
+    #     if val >= 0:
+    #         hack_writer.add_line('@{}'.format(val))
+    #         hack_writer.add_line('D=A')
+    #         hack_writer.add_line('@SP')
+    #         hack_writer.add_line('A=M')
+    #         hack_writer.add_line('M=D')
+    #     else:
+    #         hack_writer.add_line('@{}'.format(-val))
+    #         hack_writer.add_line('D=A')
+    #         hack_writer.add_line('@SP')
+    #         hack_writer.add_line('A=M')
+    #         hack_writer.add_line('M=D')
+    #         hack_writer.add_line('M=-M')
+
+
+    def handle_arithmetic(self, line, hack_writer: HackWriter, line_number):
+        if line in ['neg', 'not']:
             self.sp_minus_one(hack_writer)
-
-            self.assign_d_val_to_sp_mem(hack_writer)
-
-            self.sp_minus_one(hack_writer)
-
-            hack_writer.add_line('@SP')
-            hack_writer.add_line('A=M')
-            hack_writer.add_line('M=D+M')
-
+            self.assign_sp_mem_to_d_val(hack_writer)
+            if line == 'neg':
+                hack_writer.add_line('M=-M')
+            elif line == 'not':
+                hack_writer.add_line('M=!M')
             self.sp_incr(hack_writer)
+            return
+
+        self.sp_minus_one(hack_writer)
+
+        self.assign_sp_mem_to_d_val(hack_writer)
+
+        self.sp_minus_one(hack_writer)
+
+        hack_writer.add_line('@SP')
+        hack_writer.add_line('A=M')
+
+        if line in ['add', 'sub']:
+            if line == 'add':
+                hack_writer.add_line('M=D+M')
+            elif line == 'sub':
+                hack_writer.add_line('M=M-D')
+        elif line in ['eq', 'lt', 'gt']:
+            if line == 'eq':
+                hack_writer.add_line('D=M-D')
+                hack_writer.add_line('@EQ{}'.format(line_number))
+                hack_writer.add_line('D;JEQ')
+
+                hack_writer.add_line('@NEQ{}'.format(line_number))
+                hack_writer.add_line('0;JMP')
+
+            elif line == 'lt':
+                hack_writer.add_line('D=M-D')
+                hack_writer.add_line('@EQ{}'.format(line_number))
+                hack_writer.add_line('D;JLT')
+
+                hack_writer.add_line('@NEQ{}'.format(line_number))
+                hack_writer.add_line('0;JGE')
+
+            elif line == 'gt':
+                hack_writer.add_line('D=M-D')
+                hack_writer.add_line('@EQ{}'.format(line_number))
+                hack_writer.add_line('D;JGT')
+
+                hack_writer.add_line('@NEQ{}'.format(line_number))
+                hack_writer.add_line('0;JLE')
+
+            hack_writer.add_line('(EQ{})'.format(line_number))
+            self.assign_minus_one_to_sp_mem(hack_writer)
+            hack_writer.add_line('  @FINISHED{}'.format(line_number))
+            hack_writer.add_line('  0; JMP')
+
+            hack_writer.add_line('(NEQ{})'.format(line_number))
+            self.assign_0_to_sp_mem(hack_writer)
+
+            hack_writer.add_line('(FINISHED{})'.format(line_number))
+        elif line in ['and', 'or']:
+            if line == 'and':
+                hack_writer.add_line('D=D&M')
+
+            elif line == 'or':
+                hack_writer.add_line('D=D|M')
+
+            hack_writer.add_line('M=D')
+
+        self.sp_incr(hack_writer)
+
+
+# file_path = './StackArithmetic/SimpleAdd/SimpleAdd.vm'
+# write_file_name = 'SimpleAdd.asm'
+read_file_path = './StackArithmetic/StackTest/StackTest.vm'
+write_file_name = './StackArithmetic/StackTest/StackTest.asm'
 
 reader = Reader()
-reader.read_text_file('./StackArithmetic/SimpleAdd/SimpleAdd.asm')
+reader.read_text_file(read_file_path)
 parser = Parser(reader.vm_text)
 
 hack_writer = HackWriter()
@@ -133,6 +221,7 @@ hack_writer.add_line('M=D')
 
 while parser.has_more_commands():
     line = parser.current_line()
+    line_number = parser.current_line_number
     if parser.current_line_type() == 'stack':
         # stack
         parser.handle_stack(line, hack_writer)
@@ -140,14 +229,16 @@ while parser.has_more_commands():
 
     elif parser.current_line_type() == 'arithmetic':
         # arithmetic
-        parser.handle_arithmetic(line, hack_writer)
+        parser.handle_arithmetic(line, hack_writer, line_number)
         parser.advance()
+    else:
+        raise Exception('unknown type')
 
 hack_writer.add_line('(END)')
 hack_writer.add_line('  @END')
 hack_writer.add_line('  0;JMP')
 
 hack_writer.print()
-hack_writer.write_to_file('simple_add.asm')
+hack_writer.write_to_file(write_file_name)
 
 
